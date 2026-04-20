b6bd93a882734f2ee5b262d01758974d95378488
Luc Alexander <lucskyalex@gmail.com>
2026-04-20 20:48:16 +0200

    back to parser and codegen

--- src/main.zig
+++ src/main.zig
@@ -30,7 +30,7 @@ pub fn tokenize(mem: *const std.mem.Allocator, text: []const u8) Buffer(Token) {
 				i += 1;
 				continue;
 			},
-			open_quote, close_quote, open_word, close_word => {
+			cat, quote, unquote, open_quote, close_quote, open_word, close_word => {
 				tokens.append(Token{
 					.tag = c,
 					.value = .{
@@ -100,9 +100,6 @@ const Inst = union(enum){
 	pop_ds,
 	psh_rs,
 	pop_rs,
-	psh_cs,
-	pop_cs,
-	read_ds,
 	write_ds,
 	jmp,
 	nip,
@@ -116,6 +113,10 @@ const Inst = union(enum){
 	sub,
 	mul,
 	div,
+	cat,
+	dip,
+	quote,
+	unquote,
 	data: Word
 };
 
@@ -123,8 +124,181 @@ const ParseError = error {
 	UnexpectedToken,
 };
 
-pub fn parse(mem: *const std.mem.Allocator, tokens: []Token, k: u64, instructions: *Buffer(Inst), close_token: ?TOKEN) ParseError!u64 {
-	//TODO
+pub fn parse(mem: *const std.mem.Allocator, tokens: []Token, k: u64, instructions = Buffer(*Symbol), close_token: ?TOKEN) ParseError!u64 {
+	var i = k;
+	var defs = Map(Word).init(mem.*);
+	var def_backlog = Map(Buffer(u64)).init(mem.*);
+	while (i < tokens.len){
+		switch (tokens[i].tag){
+			open_quote => {
+				i += 1;
+				instructions.append(Inst{.jmp=undefined}) catch unreachable;
+				const save = instructions.items.len;
+				instructions.append(Inst{.data=0}) catch unreachable;
+				i = try parse(mem, tokens, i, instructions, close_quote);
+				instructions.append(Inst{ .pop_rs=undefined}) catch unreachable;
+				instructions.items[save].data = @intCast(instructions.items.len*2);
+				i += 1;
+				continue;
+			},
+			close_quote => {
+				if (close_token)|end|{
+					if (end == close_quote){
+						return i;
+					}
+				}
+				return ParseError.UnexpectedToken;
+			},
+			open_word => {
+				i += 1;
+				const loc:Word = @intCast(instructions.items.len*2);
+				const name = tokens[i];
+				if (name.tag != iden){
+					return ParseError.UnexpectedToken;
+				}
+				i += 1;
+				instructions.append(Inst{.jmp=undefined}) catch unreachable;
+				const save = instructions.items.len;
+				instructions.append(Inst{.data=0}) catch unreachable;
+				i = try parse(mem, tokens, i, instructions, close_word);
+				instructions.append(Inst{ .pop_rs=undefined}) catch unreachable;
+				defs.put(name.value.text, loc) catch unreachable;
+				instructions.items[save].data = @intCast(instructions.items.len*2);
+				if (def_backlog.get(name.value.text)) |list| {
+					for (list.items) |index| {
+						instructions.items[index].data = loc;
+					}
+				}
+				i += 1;
+				continue;
+			},
+			close_word => {
+				if (close_token)|end|{
+					if (end == close_word){
+						return i;
+					}
+				}
+				return ParseError.UnexpectedToken;
+			},
+			iden => {
+				if (std.mem.eql(u8, tokens[i].value.text, "cat")){
+					instructions.append(Inst{ .cat = undefined }) catch unreachable;
+					i += 1;
+					continue;
+				}
+				if (std.mem.eql(u8, tokens[i].value.text, "qut")){
+					instructions.append(Inst{ .quote = undefined }) catch unreachable;
+					i += 1;
+					continue;
+				}
+				if (std.mem.eql(u8, tokens[i].value.text, "unq")){
+					instructions.append(Inst{ .unquote = undefined }) catch unreachable;
+					i += 1;
+					continue;
+				}
+				if (std.mem.eql(u8, tokens[i].value.text, "dip")){
+					instructions.append(Inst{ .dip = undefined }) catch unreachable;
+					i += 1;
+					continue;
+				}
+				if (std.mem.eql(u8, tokens[i].value.text, "nip")){
+					instructions.append(Inst{ .nip = undefined }) catch unreachable;
+					i += 1;
+					continue;
+				}
+				if (std.mem.eql(u8, tokens[i].value.text, "ovr")){
+					instructions.append(Inst{ .ovr = undefined }) catch unreachable;
+					i += 1;
+					continue;
+				}
+				if (std.mem.eql(u8, tokens[i].value.text, "swp")){
+					instructions.append(Inst{ .swp = undefined }) catch unreachable;
+					i += 1;
+					continue;
+				}
+				if (std.mem.eql(u8, tokens[i].value.text, "rot")){
+					instructions.append(Inst{ .rot = undefined }) catch unreachable;
+					i += 1;
+					continue;
+				}
+				if (std.mem.eql(u8, tokens[i].value.text, "dup")){
+					instructions.append(Inst{ .dup = undefined }) catch unreachable;
+					i += 1;
+					continue;
+				}
+				if (std.mem.eql(u8, tokens[i].value.text, "cut")){
+					instructions.append(Inst{ .cut = undefined }) catch unreachable;
+					i += 1;
+					continue;
+				}
+				if (std.mem.eql(u8, tokens[i].value.text, "pop")){
+					instructions.append(Inst{ .pop_ds = undefined }) catch unreachable;
+					i += 1;
+					continue;
+				}
+				if (std.mem.eql(u8, tokens[i].value.text, "str")){
+					instructions.append(Inst{ .write_ds = undefined }) catch unreachable;
+					i += 1;
+					continue;
+				}
+				if (std.mem.eql(u8, tokens[i].value.text, "run")){
+					instructions.append(Inst{ .run = undefined }) catch unreachable;
+					i += 1;
+					continue;
+				}
+				if (std.mem.eql(u8, tokens[i].value.text, "add")){
+					instructions.append(Inst{ .add = undefined }) catch unreachable;
+					i += 1;
+					continue;
+				}
+				if (std.mem.eql(u8, tokens[i].value.text, "sub")){
+					instructions.append(Inst{ .sub = undefined }) catch unreachable;
+					i += 1;
+					continue;
+				}
+				if (std.mem.eql(u8, tokens[i].value.text, "mul")){
+					instructions.append(Inst{ .mul = undefined }) catch unreachable;
+					i += 1;
+					continue;
+				}
+				if (std.mem.eql(u8, tokens[i].value.text, "div")){
+					instructions.append(Inst{ .div = undefined }) catch unreachable;
+					i += 1;
+					continue;
+				}
+				if (defs.get(tokens[i].value.text)) |address| {
+					instructions.append(Inst{ .psh_rs = undefined}) catch unreachable;
+					instructions.append(Inst{ .jmp=undefined, }) catch unreachable;
+					instructions.append(Inst{ .data = address}) catch unreachable;
+					i += 1;
+					continue;
+				}
+				instructions.append(Inst{ .psh_rs = undefined}) catch unreachable;
+				instructions.append(Inst{ .jmp = undefined, }) catch unreachable;
+				instructions.append(Inst{ .data = 0}) catch unreachable;
+				if (def_backlog.getPtr(tokens[i].value.text)) |list| {
+					list.append(instructions.items.len-1) catch unreachable;
+				}
+				else {
+					var list = Buffer(u64).init(mem.*);
+					list.append(instructions.items.len-1) catch unreachable;
+					def_backlog.put(tokens[i].value.text, list) catch unreachable;
+				}
+				i += 1;
+				continue;
+			},
+			number => {
+				instructions.append(Inst{ .psh_ds = undefined}) catch unreachable;
+				instructions.append(Inst{ .data = tokens[i].value.numeric }) catch unreachable;
+				i += 1;
+				continue;
+			},
+			else => {
+				return ParseError.UnexpectedToken;
+			}
+		}
+	}
+	return i;
 }
 
 const OPCODE = u8;
@@ -133,10 +307,10 @@ const PSH_DS = 1;
 const POP_DS = 2;
 const PSH_RS = 3;
 const POP_RS = 4;
-const PSH_CS = 5;
-const POP_CS = 6;
-const LD = 7;
-const ST = 8;
+const ADD = 5;
+const MUL = 6;
+const SUB = 7;
+const DIV = 8;
 const JMP = 9;
 const NIP = 10;
 const ROT = 11;
@@ -144,15 +318,225 @@ const DUP = 12;
 const CUT = 13;
 const OVR = 14;
 const SWP = 15;
-const RUN = 16;
-const ADD = 17;
-const MUL = 18;
-const SUB = 19;
-const DIV = 20;
+const DIP = 16;
+const QUT = 17;
+const UNQ = 18;
+const STR = 19;
 
 pub fn code_gen(mem: *const std.mem.Allocator, instructions: Buffer(Inst)) []u8 {
-	//TODO
+	var bytes = mem.alloc(u8, instructions.items.len*2) catch unreachable;
+	var i: u64 = 0;
+	for (instructions.items) |inst| {
+		switch (inst){
+			.psh_ds => {bytes[i] = PSH_DS;},
+			.pop_ds => {bytes[i] = POP_DS;},
+			.psh_rs => {bytes[i] = PSH_RS;},
+			.pop_rs => {bytes[i] = POP_RS;},
+			.psh_cs => {bytes[i] = PSH_CS;},
+			.pop_cs => {bytes[i] = POP_CS;},
+			.read_ds => {bytes[i] = LD;},
+			.write_ds => {bytes[i] = ST;},
+			.jmp => {bytes[i] = JMP;},
+			.nip => {bytes[i] = NIP;},
+			.rot => {bytes[i] = ROT;},
+			.dup => {bytes[i] = DUP;},
+			.cut => {bytes[i] = CUT;},
+			.ovr => {bytes[i] = OVR;},
+			.swp => {bytes[i] = SWP;},
+			.run => {bytes[i] = RUN;},
+			.add => {bytes[i] = ADD;},
+			.sub => {bytes[i] = SUB;},
+			.mul => {bytes[i] = MUL;},
+			.div => {bytes[i] = DIV;},
+			.cat => {bytes[i] = CAT;},
+			.dip => {bytes[i] = DIP;},
+			.quote => {bytes[i] = QUT;},
+			.unquote => {bytes[i] = UNQ;},
+			.data => {
+				bytes[i] = @truncate((inst.data & 0xFF00) >> 8);
+				i += 1;
+				bytes[i] = @truncate(inst.data & 0xFF);
+				i += 1;
+				continue;
+			}
+		}
+		i += 2;
+	}
+	return bytes;
 }
+
+const Machine = struct {
+	mem: []u8,
+	ds: Stack,
+	rs: Stack,
+	ip: Word,
+	hp: Word,
+	running: bool,
+
+	pub fn init(mem: *const std.mem.Allocator, size: Word, ss: Word) Machine {
+		return Machine{
+			.mem = mem.alloc(u8, size) catch unreachable,
+			.ds = Stack.init(mem, ss),
+			.rs = Stack.init(mem, ss),
+			.ip = 0,
+			.running = false
+		};
+	}
+
+	pub fn load_rom(self: *Machine, loc: Word, bytes: []u8) void {
+		var i:u64 = loc;
+		while (i < loc+bytes.len){
+			self.mem[i] = bytes[i-loc];
+			i += 1;
+		}
+		self.hp = i;
+	}
+
+	pub fn run(self: *Machine, ip: Word) void {
+		self.ip = ip;
+		self.running = true;
+	}
+
+	pub fn step(self: *Machine) void {
+		if (self.running == false){
+			return;
+		}
+		switch (self.mem[self.ip]) {
+			NOP => {},
+			PSH_DS => {
+				self.ip += 2;
+				if (self.ip < self.mem.len){
+					const data = (@as(Word, @intCast(self.mem[self.ip])) << 8) + self.mem[self.ip + 1];
+					self.ds.push(data);
+				}
+			},
+			POP_DS => {
+				_ = self.ds.pop();
+			},
+			PSH_RS => {
+				self.rs.push(self.ip);
+			},
+			POP_RS => {
+				self.ip = self.rs.pop();
+			},
+			ST => {
+				const loc = self.ds.pop();
+				if (loc < self.mem.len){
+					const address = (@as(Word, @intCast(self.mem[loc])) << 8) + self.mem[loc + 1];
+					if (address < self.mem.len){
+						if (address % 2 == 0){
+							const data = self.ds.pop();
+							self.mem[address] = @truncate(data >> 8);
+							self.mem[address+1] = @truncate(data & 0xFF);
+							self.ip += 2;
+							return;
+						}
+					}
+				}
+				self.ds.push(loc);
+			},
+			JMP => {
+				self.ip += 2;
+				if (self.ip < self.mem.len){
+					const data = (@as(Word, @intCast(self.mem[self.ip])) << 8) + self.mem[self.ip + 1];
+					if (data%2==0){
+						self.ip = data;
+						return;
+					}
+				}
+			},
+			NIP => {
+				const a = self.ds.pop();
+				_ = self.ds.pop();
+				self.ds.push(a);
+			},
+			OVR => {
+				const a = self.ds.pop();
+				const b = self.ds.pop();
+				const c = self.ds.pop();
+				self.ds.push(b);
+				self.ds.push(a);
+				self.ds.push(c);
+			},
+			DUP => {
+				const a = self.ds.pop();
+				self.ds.push(a);
+				self.ds.push(a);
+			},
+			CUT => {
+				const a = self.ds.pop();
+				const b = self.ds.pop();
+				_ = self.ds.pop();
+				self.ds.push(b);
+				self.ds.push(a);
+			},
+			ROT => {
+				const a = self.ds.pop();
+				const b = self.ds.pop();
+				const c = self.ds.pop();
+				self.ds.push(a);
+				self.ds.push(c);
+				self.ds.push(b);
+			},
+			SWP => {
+				const a = self.ds.pop();
+				const b = self.ds.pop();
+				self.ds.push(a);
+				self.ds.push(b);
+			},
+			UNQ => {
+				const loc = self.ds.pop();
+				if (loc < self.mem.len){
+					const address = (@as(Word, @intCast(self.mem[loc])) << 8) + self.mem[loc + 1];
+					if (address < self.mem.len){
+						const new_ip = (@as(Word, @intCast(self.mem[address])) << 8) + self.mem[address + 1];
+						if (address%2 == 0){
+							self.rs.push(self.ip);
+							self.ip = new_ip;
+							return;
+						}
+					}
+				}
+				self.ds.push(loc);
+			},
+			ADD => {
+				const a = self.ds.pop();
+				const b = self.ds.pop();
+				const c = a +% b;
+				self.ds.push(c);
+			},
+			MUL => {
+				const a = self.ds.pop();
+				const b = self.ds.pop();
+				const c = a *% b;
+				self.ds.push(c);
+			},
+			SUB => {
+				const a = self.ds.pop();
+				const b = self.ds.pop();
+				const c = a -% b;
+				self.ds.push(c);
+			},
+			DIV => {
+				const a = self.ds.pop();
+				const b = self.ds.pop();
+				const c = a / b;
+				self.ds.push(c);
+			},
+			DIP => {
+
+			},
+			QUT => {
+
+			},
+			CAT => {
+				
+			},
+			else => { }
+		}
+		self.ip += 2;
+	}
+};
 
 pub fn get_contents(mem: *const std.mem.Allocator, filename: []const u8) ![]u8 {
 	var infile = std.fs.cwd().openFile(filename, .{}) catch |err| {
@@ -203,3 +587,4 @@ pub fn main() !void {
 }
 
 
+//TODO multicore
